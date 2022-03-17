2c3bf0ed9e727f830e2a649cf8bb20b2edf3cf88
JianyiMao <yyc5009@autuni.ac.nz>
2022-03-18 02:39:43 +1300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const server = express();
+
+
+const dotenv = require("dotenv")
+
+
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const articles = require("./dummydata/articles")
+
+
+
+server.get('/api/articles', (req,res) => {
+    res.json(articles);
+});
+
+server.get('/api/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+});
+
+
+server.get('/',(req,res)=>{
+    res.send("API is running")
+})
+
+server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
